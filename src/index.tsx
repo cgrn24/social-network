0d07b5f2a03b461fcc9985e20b679b94c96fff56
cgrn24 <stlhnt42@gmail.com>
2023-02-24 23:29:53 +0700

    profileReducer immutable refactor

--- src/index.tsx
+++ src/index.tsx
@@ -7,19 +7,11 @@ import './index.css'
 import { RootStateType } from './redux/state'
 import { RootStoreType, store } from './redux/store'
 
-const renderEntireTree = (state: RootStateType) => {
-  ReactDOM.render(
-    <BrowserRouter>
-      <Provider store={store}>
-        <App store={store} />
-      </Provider>
-    </BrowserRouter>,
-    document.getElementById('root')
-  )
-}
-
-renderEntireTree(store.getState())
-store.subscribe(() => {
-  const state = store.getState()
-  renderEntireTree(state)
-})
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App store={store} />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('root')
+)
