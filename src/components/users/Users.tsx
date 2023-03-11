e8022aac25df40ecb25fcf1b8e5783811c9138fa
cgrn24 <stlhnt42@gmail.com>
2023-03-11 23:54:09 +0700

    follow unfollow refactor

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import { NavLink } from 'react-router-dom'
 import { UsersType } from '../../redux/usersReducer'
 import style from './Users.module.css'
@@ -45,7 +46,31 @@ export const Users = (props: UsersFCPropsType) => {
               </NavLink>
             </div>
             <div>
-              {u.followed ? <button onClick={() => props.unfollow(u.id)}>Unfollow</button> : <button onClick={() => props.follow(u.id)}>Follow</button>}
+              {u.followed ? (
+                <button
+                  onClick={() =>
+                    axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/$(u.id)`, { withCredentials: true }).then((res: any) => {
+                      if (res.data.resultCode === 0) {
+                        props.unfollow(u.id)
+                      }
+                    })
+                  }
+                >
+                  Unfollow
+                </button>
+              ) : (
+                <button
+                  onClick={() =>
+                    axios.post(`https://social-network.samuraijs.com/api/1.0/follow/$(u.id)`, {}, { withCredentials: true }).then((res: any) => {
+                      if (res.data.resultCode === 0) {
+                        props.follow(u.id)
+                      }
+                    })
+                  }
+                >
+                  Follow
+                </button>
+              )}
             </div>
           </span>
           <span>
