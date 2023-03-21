ec32e5d52832a406a129d7d07cce24b22321ce7e
cgrn24 <stlhnt42@gmail.com>
2023-03-21 20:26:20 +0700

    redux-form for login

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,5 +1,32 @@
 import React from 'react'
+import { Field, reduxForm } from 'redux-form'
 
 export const Login = () => {
-  return <div>Login</div>
+  const onSubmit = (formData) => {
+    console.log(formData)
+  }
+  return <LoginReduxForm />
 }
+const LoginForm = (props) => {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <div>
+        <Field placeholder={'Login'} component={'input'} name={'login'} />
+      </div>
+      <div>
+        <Field placeholder={'Password'} component={'input'} name={'password'} />
+      </div>
+      <div>
+        <Field type={'checkbox'} component={'input'} name={'rememberMe'} />
+        remember me
+      </div>
+      <div>
+        <button>Login</button>
+      </div>
+    </form>
+  )
+}
+
+const LoginReduxForm = reduxForm({
+  form: 'login',
+})(LoginForm)
--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -4,6 +4,7 @@ import profileReducer, { ProfileActionsType } from './profileReducer'
 import { applyMiddleware, combineReducers, createStore, Store } from 'redux'
 import { UsersPageType, usersReducer } from './usersReducer'
 import { AuthActionsType, authReducer } from './authReducer'
+import { reducer as formReducer } from 'redux-form'
 import thunk from 'redux-thunk'
 
 const reducers = combineReducers({
@@ -12,10 +13,12 @@ const reducers = combineReducers({
   sidebar: sidebarReducer,
   usersPage: usersReducer,
   auth: authReducer,
+  form: formReducer,
 })
 
 export type RootStoreType = ReturnType<typeof reducers>
 export type ActionsType = ProfileActionsType | DialogsActionsType | UsersPageType | AuthActionsType
 export type StoreType = Store<RootStoreType, ActionsType>
+// type FormReducerType = ReturnType<typeof formReducer>
 
 export const store = createStore(reducers, applyMiddleware(thunk))
